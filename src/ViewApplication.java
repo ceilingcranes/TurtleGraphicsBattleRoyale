db38466fb9f99f9a4cd36c8e27c14499601afec5
Maxine Hartnett <maxinehartnett@gmail.com>
2018-10-28 22:40:49 -0600

    GUI updates

--- src/ViewApplication.java
+++ src/ViewApplication.java
@@ -8,7 +8,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
@@ -19,11 +21,11 @@ import javax.swing.*;
 
 public class ViewApplication extends Application {
 
-    private final IntegerProperty numPlayers = new SimpleIntegerProperty();
+    private final IntegerProperty numPlayers = new SimpleIntegerProperty(1);
 
     class NumPlayersButton implements EventHandler<ActionEvent> {
         private final int number;
-        public NumPlayersButton(int np){
+        private NumPlayersButton(int np){
             number = np;
         }
         @Override
@@ -32,8 +34,7 @@ public class ViewApplication extends Application {
         }
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    private Pane startScreenSceneGraph(Stage primaryStage){
         int max_players  = 4;
 
         Text title = new Text();
@@ -65,11 +66,9 @@ public class ViewApplication extends Application {
 
         Text number = new Text();
         Button submitButton = new Button("Submit");
-        submitButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                number.setText(numPlayers.toString());
-            }
+        submitButton.setOnAction((event)-> {
+            number.setText(numPlayers.toString());
+            playerCreationScene(primaryStage);
         });
 
         submitButton.setAlignment(Pos.CENTER);
@@ -77,10 +76,61 @@ public class ViewApplication extends Application {
         GridPane.setConstraints(number, 0, 2);
         GridPane.setConstraints(submitButton, 0, 3);
 
+        return root;
+    }
+
+    private void startScreenScene(Stage primaryStage){
+        GridPane start = (GridPane) startScreenSceneGraph(primaryStage);
+        updateStage(primaryStage, start, "Start");
+    }
+
+    private Pane playerCreationSceneGraph(Stage primaryStage){
+        Button backButton = new Button("Back");
+        backButton.setOnAction((event)->{
+           startScreenScene(primaryStage);
+        });
+
+        Text text = new Text();
+
+        GridPane playerPane = new GridPane();
+        TextField nameField = new TextField();
+        GridPane.setConstraints(nameField,0,0);
+        playerPane.getChildren().add(nameField);
+
+        Button addPlayer = new Button("Add Player");
+        GridPane.setConstraints(addPlayer, 0, 1);
+        addPlayer.setOnAction((event)-> {
+            text.setText(nameField.getText());
+        });
+
+        playerPane.getChildren().add(addPlayer);
+        playerPane.setAlignment(Pos.CENTER);
+
+        GridPane root = new GridPane();
+        GridPane.setConstraints(text,0,0);
+        GridPane.setConstraints(backButton, 1,0);
+        GridPane.setConstraints(playerPane, 0,1);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(text,backButton,playerPane);
+
+        return root;
+    }
+
+    private void playerCreationScene(Stage primaryStage){
+        Pane sceneGraph = playerCreationSceneGraph(primaryStage);
+        updateStage(primaryStage, sceneGraph, "Player Update");
+    }
+
+    private void updateStage(Stage primaryStage, Pane root, String title){
         Scene scene = new Scene(root, 1000, 1000);
-        primaryStage.setTitle("Test");
+        primaryStage.setTitle(title);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        startScreenScene(primaryStage);
     }
 
     public static void main(String[] args) {
